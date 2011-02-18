a3b42441583cf95e6aa084899e5b892861d9a35b
Ian Yang <doit.ian@gmail.com>
2011-02-18 16:00:05 +0800

    enable yasnippet in markdown

--- lisp/iy-markdown.el
+++ lisp/iy-markdown.el
@@ -3,6 +3,7 @@
 (add-hook 'markdown-mode-hook 'iy-markdown-mode-init)
 (defun iy-markdown-mode-init ()
   (turn-on-auto-fill)
-  (flyspell-mode 1))
+  (flyspell-mode 1)
+  (when (fboundp 'yas/fix-keybindings) (yas/fix-keybindings)))
 
 (provide 'iy-markdown)
