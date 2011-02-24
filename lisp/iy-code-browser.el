559264193b8672410ee2b5551ca821a6ac5f821a
Ian Yang <me@iany.me>
2011-02-24 18:55:21 +0800

    add package etags-select

--- lisp/iy-code-browser.el
+++ lisp/iy-code-browser.el
@@ -1,3 +1,7 @@
 (push 'full-ack el-get-sources)
+(push '(:name etags-select :type elpa) el-get-sources)
+
+(global-set-key "\M-?" 'etags-select-find-tag-at-point)
+(global-set-key "\M-." 'etags-select-find-tag)
 
 (provide 'iy-code-browser)
