72a7498f4b34e4df7c5b8eafe78a570325828f45
Ian Yang <me@iany.me>
2011-02-28 22:53:02 +0800

    fix quote characters copied from web

--- lisp/iy-code-browser.el
+++ lisp/iy-code-browser.el
@@ -18,17 +18,17 @@
     (cscope-tell-ignore-case))
   (defun cscope-tell-ignore-case ()
     (interactive)
-    (message “Cscope Ignore Case (%s)”
-              (if cscope-ignore-case “Enable” “Disable”)))
+    (message "Cscope Ignore Case (%s)"
+              (if cscope-ignore-case "Enable" "Disable")))
 
-  (define-key cscope:map “\C-csv” ‘cscope-toggle-case)
-  (define-key cscope:map “\C-csV” ‘cscope-tell-ignore-case)
-  (define-key cscope-list-entry-keymap “v” ‘cscope-toggle-case)
-  (define-key cscope-list-entry-keymap “V” ‘cscope-tell-ignore-case)
+  (define-key cscope:map "\C-csv" 'cscope-toggle-case)
+  (define-key cscope:map "\C-csV" 'cscope-tell-ignore-case)
+  (define-key cscope-list-entry-keymap "v" 'cscope-toggle-case)
+  (define-key cscope-list-entry-keymap "V" 'cscope-tell-ignore-case)
 
   (defadvice cscope-call (before ignore-case activate)
-    “ignore case in cscope search”
+    "ignore case in cscope search"
      (when cscope-ignore-case
-       (ad-set-arg 1 (cons “-C” (ad-get-arg 1))))))
+       (ad-set-arg 1 (cons "-C" (ad-get-arg 1))))))
 
 (provide 'iy-code-browser)
