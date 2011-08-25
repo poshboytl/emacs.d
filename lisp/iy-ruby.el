12bc24ac5591bc0c6d5243f9c9a8dc0a2ba6313f
Ian Yang <me@iany.me>
2011-08-26 01:54:47 +0800

    enable ruby-mode in various ruby dsl config file

--- lisp/iy-ruby.el
+++ lisp/iy-ruby.el
@@ -2,6 +2,9 @@
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.thor\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile\\'" . ruby-mode))
 
 (push '(:name rinari
               :after (lambda () (load (concat el-get-dir "rinari/util/ruby-mode"))))
