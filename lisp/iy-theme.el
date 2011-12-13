14f044ca5b85aeff04817499815c63104ffbd91e
Ian Yang <me@iany.me>
2011-12-14 00:52:20 +0800

    reset colors when open new frame from daemon

--- lisp/iy-theme.el
+++ lisp/iy-theme.el
@@ -11,6 +11,15 @@
 (set-frame-font iy-frame-font)
 (set-fontset-font "fontset-default" 'chinese-gbk iy-frame-font-chinese)
 
+(add-hook 'after-make-frame-functions
+          (lambda
+            (new-frame)
+            (select-frame new-frame)
+            (when (and window-system (eq solarized-colors-index 3))
+              (setq solarized-colors-index 1)
+              (create-solarized-theme dark)
+              (color-theme-solarized-dark))))
+
 (setq frame-title-format '(buffer-file-name "Emacs: %b (%f)" "Emacs: %b"))
 
 (provide 'iy-theme)
