3976f6f7e23fb461a5ede5728e2cf40df05b73a4
Ian Yang <me@iany.me>
2012-01-12 19:53:33 +0800

    fix regexp

--- lisp/modes/iy-ruby-mode.el
+++ lisp/modes/iy-ruby-mode.el
@@ -58,6 +58,6 @@
 
 (font-lock-add-keywords
  'ruby-mode
- '(("\\(\\b\\sw[_a-zA-Z0-9]*:\\)\\(?:\\s-\\|$)" (1 font-lock-constant-face))))
+ '(("\\(\\b\\sw[_a-zA-Z0-9]*:\\)\\(?:\\s-\\|$\\)" (1 font-lock-constant-face))))
 
 (provide 'iy-ruby-mode)
