1fac2491368479ec3ba5e96278a67fd81e95373d
Ian Yang <me@iany.me>
2012-01-13 13:09:18 +0800

    add sql mode config

---
+++ lisp/modes/iy-sql-mode.el
@@ -0,0 +1,17 @@
+(require 'iy-dep)
+
+;; http://www.emacswiki.org/emacs/SqlMode
+(defun my-sql-save-history-hook ()
+  (let ((lval 'sql-input-ring-file-name)
+        (rval 'sql-product))
+    (if (symbol-value rval)
+        (let ((filename 
+               (concat iy-data-dir
+                       (symbol-name (symbol-value rval))
+                       "-history.sql")))
+          (set (make-local-variable lval) filename))
+      (error
+       (format "SQL history will not be saved because %s is nil"
+               (symbol-name rval))))))
+
+(add-hook 'sql-interactive-mode-hook 'my-sql-save-history-hook)
