66966bc1f85fb32455dc64f82bbc6a6fdaa681bc
Ian Yang <me@iany.me>
2012-03-28 13:37:06 +0800

    setup zencoding in html-mode

--- lisp/iy-web.el
+++ lisp/iy-web.el
@@ -11,16 +11,16 @@
  el-get-sources)
 
 (defun iy-el-get-after-nxhtml ()
-  (defun iy-nxhtml-mode-init ()
-    (zencoding-mode t)
-    (local-set-key (kbd "<C-return>") 'zencoding-expand-line))
-  (add-hook 'nxhtml-mode-hook 'iy-nxhtml-mode-init)
-
   ;; revert some mode
   (add-to-list 'auto-mode-alist '("\\.md\\.erb\\'" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.html\\.md\\.erb\\'" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.html\\.md\\'" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.html.haml\\'" . haml-mode))
   (add-to-list 'auto-mode-alist '("\\.eco\\'" . html-mode)))
+
+(defun iy-html-mode-init ()
+  (zencoding-mode t)
+  (local-set-key (kbd "<C-return>") 'zencoding-expand-line))
+(add-hook 'html-mode-hook 'iy-html-mode-init)
 
 (provide 'iy-web)
