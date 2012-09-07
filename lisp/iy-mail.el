d1125ee9e60033eaf7333adedd5cc34f51692857
Ian Yang <me@iany.me>
2012-09-07 15:16:57 +0800

    add commented code to load mu4e

--- lisp/iy-mail.el
+++ lisp/iy-mail.el
@@ -1,3 +1,8 @@
 (add-hook 'mail-mode-hook 'flyspell-mode)
 
+;; (when (require 'mu4e nil t)
+;;   (global-set-key (kbd "<menu>") 'mu4e)
+;;   (custom-set-variables
+;;    '(mu4e-maildir (expand-file-name "~/Mail"))))
+
 (provide 'iy-mail)
