0e951d67011088bf5573a9eac28dc8056fe17d54
Ian Yang <me@iany.me>
2012-09-10 15:29:28 +0800

    send mail using msmtp

--- lisp/iy-mail.el
+++ lisp/iy-mail.el
@@ -1,8 +1,16 @@
 (add-hook 'mail-mode-hook 'flyspell-mode)
 
-;; (when (require 'mu4e nil t)
-;;   (global-set-key (kbd "<menu>") 'mu4e)
-;;   (custom-set-variables
-;;    '(mu4e-maildir (expand-file-name "~/Mail"))))
+(global-set-key (kbd "<menu>") 'mu4e)
+
+(autoload 'mu4e "mu4e" nil t)
+(eval-after-load 'mu4e
+  (progn
+    (custom-set-variables
+     '(mu4e-maildir (expand-file-name "~/Mail")))))
+
+(setq message-send-mail-function 'message-send-mail-with-sendmail)
+(setq sendmail-program "msmtp")
+(setq message-sendmail-extra-arguments '("-a" "ianyme"))
+(setq mail-host-address "iany.me")
 
 (provide 'iy-mail)
