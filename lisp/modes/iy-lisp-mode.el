21547ea522a7925cdd1d1af1292cf1cfa2e32aa1
Ian Yang <me@iany.me>
2013-01-19 06:00:58 +0800

    paredit-wrap-round-from-behind

--- lisp/modes/iy-lisp-mode.el
+++ lisp/modes/iy-lisp-mode.el
@@ -1,5 +1,15 @@
+(defun paredit-wrap-round-from-behind ()
+  (interactive)
+
+  (forward-sexp -1)
+  (paredit-wrap-round)
+  (insert " ")
+  (forward-char -1))
+
 (defun iy-lisp-mode-init ()
-  (when (fboundp 'paredit-mode) (paredit-mode))
+  (when (fboundp 'paredit-mode)
+    (paredit-mode)
+    (define-key paredit-mode-map (kbd "M-)") 'paredit-wrap-round-from-behind))
   (local-set-key (kbd "<return>") 'reindent-then-newline-and-indent)
   (local-set-key (kbd "C-j") 'newline))
 
