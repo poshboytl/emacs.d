af166e6ebb143a097d319725ac854577c2db1d3a
Ian Yang <me@iany.me>
2013-02-28 20:52:34 +0800

    allow select a tmux window and pane to send command

--- lisp/3rdparty/tmux-send.el
+++ lisp/3rdparty/tmux-send.el
@@ -20,7 +20,12 @@
 (defun tmux-select (session)
   "Select a tmux session."
   (interactive
-   (list (completing-read "Select a tmux session: " (tmux-list))))
+   (list (concat
+          (completing-read "Select a tmux session: " (tmux-list))
+          (let ((window (read-from-minibuffer "Select a window (current): ")))
+            (if (and window (> (length window) 0))
+                (concat ":" window)
+              "")))))
   (setq tmux-session session))
 
 ;;;###autoload
@@ -31,15 +36,14 @@ block of text to the selected tmux session."
   (when (not tmux-session)
     (call-interactively 'tmux-select))
   (let ((selected (if (region-active-p)
-                      (concat
-                       (buffer-substring-no-properties (region-beginning) (region-end))
-                       "\n")
+                      (buffer-substring-no-properties (region-beginning) (region-end))
                     (read-from-minibuffer "Command: " nil nil nil tmux-send-hist)))
         (tmpfile (make-temp-file "tmux-send.")))
     (when (region-active-p)
       (add-to-history 'tmux-send-hist selected))
     (with-temp-file tmpfile
-      (insert selected))
+      (insert selected)
+      (insert "\n"))
     (call-process "tmux" nil nil nil
                   "load-buffer" tmpfile ";"
                   "paste-buffer" "-t" tmux-session ";")
